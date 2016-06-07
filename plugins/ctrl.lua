f07ad7f4df0c63ddbfe8e77a9df606fea8705aff
umbrellaTG <abbasgh912@gmail.com>
2016-06-08 02:27:35 +0430

    Update ctrl.lua

--- plugins/ctrl.lua
+++ plugins/ctrl.lua
@@ -26,13 +26,13 @@ local function enable_channel(receiver, to_id)
 	end
 
 	if _config.disabled_channels[receiver] == nil then
-		return 'Bot was on :D'
+		return 'online'
 	end
 	
 	_config.disabled_channels[receiver] = false
 
 	save_config()
-	return 'power on'
+	return 'online'
 end
 
 local function disable_channel(receiver, to_id)
@@ -43,7 +43,7 @@ local function disable_channel(receiver, to_id)
 	_config.disabled_channels[receiver] = true
 
 	save_config()
-	return 'ok\noff'
+	return 'offline'
 end
 
 local function pre_process(msg)
